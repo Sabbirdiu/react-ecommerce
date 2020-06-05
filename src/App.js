42f47c892c07d5bf3a0a4c6e378efa4c7e054f22
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-05 23:56:53 +0600

    update the Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // pages
-
+import About from './components/pages/About';
+import Cart from './components/pages/Cart';
+import Checkout from './components/pages/Checkout';
+import Error from './components/pages/Error';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import ProductDetails from './components/pages/ProductDetails';
+import Products from './components/pages/Products';
 //Header
 import Header from './components/Header';
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/Products' component={Products} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/Cart' component={Cart} />
+          <Route exact path='*' component={Error} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ import {
   DropdownItem,
   NavbarText,
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,30 +22,50 @@ const Header = (props) => {
 
   return (
     <div>
-      <Navbar color='light' light expand='md'>
-        <NavbarBrand href=''>reactstrap</NavbarBrand>
+      <Navbar color='secondary' light expand='md'>
+        <Link className='text-light nav-link' to='/'>
+          FashionTech
+        </Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className='mr-auto' navbar>
             <NavItem>
-              <NavLink href=''>Components</NavLink>
+              <Link className='ml-5 text-light nav-link' to='/'>
+                Home
+              </Link>
             </NavItem>
             <NavItem>
-              <NavLink href=''>GitHub</NavLink>
+              <Link className='ml-5 text-light nav-link' to='products'>
+                Products
+              </Link>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            <NavItem>
+              <Link className='ml-5 text-light nav-link' to='about'>
+                About
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className='ml-5 text-light nav-link' to='cart'>
+                Card
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className='ml-5 text-light nav-link' to='login'>
+                Login
+              </Link>
+            </NavItem>
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
+          <form class='form-inline my-2 my-lg-0'>
+            <input
+              class='form-control mr-sm-2'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+            />
+            <button class='btn btn-outline-success my-2 my-sm-0 ' type='submit'>
+              Search
+            </button>
+          </form>
         </Collapse>
       </Navbar>
     </div>
